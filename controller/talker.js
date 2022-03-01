b7e9b7f88452e534b4da333021774debd6a04751
Igor Campos Moreira <igorcampos200@gmail.com>
2022-03-01 10:35:05 -0300

    OK: Req 6

--- controller/talker.js
+++ controller/talker.js
@@ -6,8 +6,9 @@ const fs = require('fs').promises;
 const router = express.Router();
 const OK = 200;
 const CREATED = 201;
-const INTERNAL_ERRO = 500;
+const NO_CONTENT = 204;
 const NOT_FOUND = 404;
+const INTERNAL_ERRO = 500;
 
 const { tokenValidation,
   nameValidation, 
@@ -79,12 +80,35 @@ talkValuesValidation, async (req, res) => {
     const talkers = JSON.parse(data);
   
     const found = talkers.findIndex((talker) => talker.id === parseInt(id, 10));
+
+    if (found === -1) return req.status(NOT_FOUND).send({ message: 'NOT FOUND' });
     
     talkers[found] = { ...talkers[found], name, age, talk }; 
   // aqui estou dando um spread no talkes para manter o id original
     await fs.writeFile('./talker.json', JSON.stringify(talkers));
   
     return res.status(OK).send(talkers[found]);
+  } catch (err) {
+    return res.status(INTERNAL_ERRO).end('erro no banco de dados');
+  }
+});
+
+router.delete('/:id', tokenValidation, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, age, talk } = req.body;
+    const data = await (fs.readFile(talkerJson));
+    const talkers = JSON.parse(data);
+  
+    const found = talkers.findIndex((talker) => talker.id === parseInt(id, 10));
+
+    if (found === -1) return req.status(NOT_FOUND).send({ message: 'NOT FOUND' });
+    
+    talkers.splice(found, 1);
+  
+    await fs.writeFile('./talker.json', JSON.stringify(talkers));
+  
+    return res.status(NO_CONTENT).end();
   } catch (err) {
     return res.status(INTERNAL_ERRO).end('erro no banco de dados');
   }
